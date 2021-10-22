d0cde0aa92ea1851aead0d1f39f41b1761e64da3
Dylan Estrada <dylan@hotmail.com>
2021-10-22 17:24:44 -0500

    programacion orientada a objetos c sharp concepts

---
+++ POO/POO/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace POO
+   
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            realizarTarea();
+
+            var miVariable = new { Nombre = "Dylan", Edad = 20 };
+            
+            
+        }
+
+
+        static void realizarTarea()
+        {
+            //TODO: 
+            Punto destino = new Punto(5,5);
+            Punto origen= new Punto();
+
+            double distancia = origen.Distancia(destino);
+            Console.WriteLine(distancia);
+
+            Console.WriteLine("cantidad de objetos "+Punto.ContadorObj());
+        }
+
+    }
+
+ 
+
+}
---
+++ POO/POO/Punto.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POO
+{
+    class Punto
+    {
+
+        private int x, y;
+
+        public Punto(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+            contadorObj++;
+        }
+
+        public Punto()
+        {
+            this.x = 0;
+            this.y = 0;
+            contadorObj++;
+        }
+
+        public double Distancia(Punto otroPunto)
+        {
+            int xDif = this.x - otroPunto.x;
+            int yDif = this.y - otroPunto.y;
+
+            double distanciaPuntos = Math.Sqrt(Math.Pow(xDif, 2)+ Math.Pow(yDif, 2));
+            return distanciaPuntos;
+        }
+        
+
+        public static int ContadorObj()
+        {
+            return contadorObj;
+        }
+
+        private static int contadorObj = 0;
+
+    }
+}
